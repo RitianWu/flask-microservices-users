912f79f94d079eda8ab6bcc8ab767441e1567fbb
git <wh198910@gmail.com>
2017-12-31 16:15:20 +0800

    1. add conver

--- manage.py
+++ manage.py
@@ -1,10 +1,20 @@
 # manage.py
 
 import unittest
+import coverage
 
 from flask_script import Manager
 from project import create_app, db
 from project.api.models import User
+
+COV = coverage.coverage(
+	branch=True,
+	include='project/*',
+	omit=[
+		'project/tests/*'
+	]
+)
+COV.start()
 
 app = create_app()
 manager = Manager(app)
@@ -12,29 +22,45 @@ manager = Manager(app)
 
 @manager.command
 def test():
-    """Runs the tests without code coverage."""
-    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
+	"""Runs the tests without code coverage."""
+	tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+	result = unittest.TextTestRunner(verbosity=2).run(tests)
+	if result.wasSuccessful():
+		return 0
+	return 1
+
+
+@manager.command
+def cov():
+	"""Runs the unit tests with coverage."""
+	tests = unittest.TestLoader().discover('project/tests')
+	result = unittest.TextTestRunner(verbosity=2).run(tests)
+	if result.wasSuccessful():
+		COV.stop()
+		COV.save()
+		print('Coverage Summary:')
+		COV.report()
+		COV.html_report()
+		COV.erase()
+		return 0
+	return 1
 
 
 @manager.command
 def recreate_db():
-    """Recreates a database."""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
+	"""Recreates a database."""
+	db.drop_all()
+	db.create_all()
+	db.session.commit()
 
 
 @manager.command
 def seed_db():
-    """Seeds the database."""
-    db.session.add(User(username='测试1', email='test1@gmail.com'))
-    db.session.add(User(username='测试2', email='test2@gmail.com'))
-    db.session.commit()
+	"""Seeds the database."""
+	db.session.add(User(username='测试1', email='test1@gmail.com'))
+	db.session.add(User(username='测试2', email='test2@gmail.com'))
+	db.session.commit()
 
 
 if __name__ == '__main__':
-    manager.run()
+	manager.run()
